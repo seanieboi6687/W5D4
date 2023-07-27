9fde5b25a1ed5d26527dbf355ce067dab4bd3b8d
Sean <pca.sean.jeun@gmail.com>
2023-07-27 18:40:40 -0400

    course 'enrolled_student' method association built

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -13,6 +13,10 @@ class Course < ApplicationRecord
     has_many :enrollments,
     primary_key: :id,
     foreign_key: :course_id,
-    class_name: :Enrollments
+    class_name: :Enrollment
 
+    has_many :enrolled_students,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
 end
