b5a233e0e77fb9af2096097d3ae2fe4bc1f6a82c
Sean <pca.sean.jeun@gmail.com>
2023-07-27 18:33:19 -0400

    debugged user.rb

--- practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
@@ -9,15 +9,13 @@
 #  updated_at :datetime         not null
 #
 class Enrollment < ApplicationRecord
-    belongs_to(:student, {
+    belongs_to :student,
         primary_key: :id,
         foreign_key: :student_id,
         class_name: :User
-    })
 
-    belongs_to(:course, {
+    belongs_to :course,
         primary_key: :id,
         foreign_key: :course_id,
         class_name: :Course
-    })
 end
