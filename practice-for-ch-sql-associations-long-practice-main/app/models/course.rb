45286075a3ea5080414d7eb89a830896c70a939c
Sean <pca.sean.jeun@gmail.com>
2023-07-27 18:51:37 -0400

    instructor association added; optional null boolean added to prerequisite association

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -23,5 +23,11 @@ class Course < ApplicationRecord
     belongs_to :prerequisite,
     primary_key: :id,
     foreign_key: :prereq_id,
-    class_name: :Course
+    class_name: :Course,
+    optional: true
+
+    belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
 end
